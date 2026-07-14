e5b3b738c07c3769c1d280a0edf85ec62e35397f
Joshua Sintos <sintosjm@gmail.com>
2026-07-14 08:17:49 +0800

    Github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,8 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   // IMPORTANT: Change this to your deployed site URL
-  site: "https://your-domain.com",
+  site: "https://jsintos.github.io",
+  // base: '/astro',
 
   vite: {
     plugins: [tailwindcss()],
